e62420a0b7851afbebc08f3d97e75717a1a8837d
nahom <nahomasnake12@gmail.com>
2025-07-21 15:47:09 +0300

    feat: Add basic categories (Service, Jobs, Buy and Sell)

---
+++ migrations/000006_add_basic_categories.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM categories WHERE slug IN ('service', 'jobs', 'buy-and-sell');
---
+++ migrations/000006_add_basic_categories.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (id, name, slug, description, created_at, updated_at) VALUES
+    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Service', 'service', 'Categories related to various services.', NOW(), NOW()),
+    ('b0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Jobs', 'jobs', 'Categories related to job postings.', NOW(), NOW()),
+    ('c0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Buy and Sell', 'buy-and-sell', 'Categories for buying and selling items.', NOW(), NOW());
